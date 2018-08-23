a87fd66acf3cb95bdb5e1ff5b2f4f48b624220c4
Ajay Thakkar <40303519+AjayKThakkar@users.noreply.github.com>
2018-08-23 16:01:39 -0400

    Edited Main js for readability

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import './main.scss';
+
 import 'babel-polyfill';
+
 import React from 'react';
+
 import ReactDOM from 'react-dom';
+
 import TwitterFeed from './containers/TwitterFeed';
+
 import data from './constants/data';
 
 ReactDOM.render(
